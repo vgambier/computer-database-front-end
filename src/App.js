285af2c28ffefaed00d53067a64b22389b23adc2
Victor Gambier <vgambier@excilys.com>
2020-08-10 17:51:52 +0200

    Now showing all computer data as a table
    Also changed the way the server url is used to reflect changes made in the back-end

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export const server_url = "http://"+SERVER_INFO.ip_address +":" +SERVER_INFO.por
 
 function App() {
 
-    const [{ data }] = useAxios(`${server_url}/page/1`); // Connecting to the server (back-end)
+    const [{ data }] = useAxios(`${server_url}/computers/page/1`); // Connecting to the server (back-end)
     const [computers, setComputers] = useState(data); // Grabbing data from the dataset
 
     useEffect(() => setComputers(data), [data]);
@@ -17,11 +17,38 @@ function App() {
 
         <div className="App">
 
-            Welcome to CDB! Here is a bunch of computers:
+            Welcome to CDB!
 
-            {computers && computers.map( // We need to check that computers is not undefined because of asynchronicity
-                computer => <Computer computer={computer}/>
-            )}
+            <table>
+                <thead>
+                    <tr>
+                        <th class="editMode">
+                            <input type="checkbox" id="selectall" />
+                            <span>
+                                -
+                                <a href="#" id="deleteSelected" onclick="$.fn.deleteSelected();">
+                                    <i class="fa fa-trash-o fa-lg"></i>
+							    </a>
+						</span></th>
+
+                        <th>Name</th>
+                        <th>Introduced</th>
+                        <th>Discontinued</th>
+                        <th>Company</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        {computers && computers.map( // We need to check that computers is not undefined because of asynchronicity
+                            computer => <Computer computer={computer}/>
+                        )}
+                    </tr>
+                </tbody>
+
+            </table>
+
+
 
         </div>
   );
--- src/Computer.js
+++ src/Computer.js
@@ -5,10 +5,26 @@ function Computer(props) {
     const [computer, setComputer] = useState(props.computer);
     const {id, name, introduced, discontinued, company} = computer;
 
+    function printIntroduced(date) {
+        return date.introduced != undefined ? date.introduced.dayOfMonth+"-"+date.introduced.month+"-"+date.introduced.year : "";
+    }
+    function printDiscontinued(date) {
+        return date.discontinued != undefined ? date.discontinued.dayOfMonth+"-"+date.discontinued.month+"-"+date.discontinued.year : "";
+    }
+    function printCompany(company) {
+        return company.company != undefined ? company.company.name : "";
+    }
+
     return (
 
         <div className="Computer">
-            {name}
+
+            <td className="editMode"> <input type="checkbox" name="cb" className="cb" value={id}/> </td>
+            <td> {name} </td>
+            <td> {printIntroduced({introduced})} </td>
+            <td> {printDiscontinued({discontinued})} </td>
+            <td> {printCompany({company})} </td>
+
         </div>
 
     );
