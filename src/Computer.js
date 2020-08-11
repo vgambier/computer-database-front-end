516384db73677820b48b1360f7da319588769ba8
Victor Gambier <vgambier@excilys.com>
2020-08-11 17:36:48 +0200

    Dates are no longer JSON objects but strings

--- src/Computer.js
+++ src/Computer.js
@@ -9,27 +9,11 @@ function Computer(props) {
     const {id, name, introduced, discontinued, company} = computer;
 
     function printDate(date) {
-        return date !== null ? date.dayOfMonth + "-" + date.month + "-" + date.year : "";
+        return date !== null && date != "" ? date.dayOfMonth + "-" + date.month + "-" + date.year : "";
     }
 
     function printCompany(company) {
         return company.company !== null ? company.company.name : "";
-    }
-
-    function dateToJSON(string) {
-
-        // TODO if null send "", otherwise send well-formatted JSON
-
-        if (true) {
-            return "";
-        } else {
-            const date = new Date(string);
-            return {
-                "year": date.getFullYear(),
-                "month": date.getMonth() + 1,
-                "dayOfMonth": date.getUTCDate()
-            };
-        }
     }
 
     function companyToJSON(company) {
@@ -61,9 +45,9 @@ function Computer(props) {
                 <>
                     <Input defaultValue={name} onChange={elt => setComputer({...computer, name: elt.target.value})}/>
                     <Input defaultValue={printDate({introduced}.introduced)}
-                           onChange={elt => setComputer({...computer, introduced: dateToJSON(elt.target.value)})}/>
+                           onChange={elt => setComputer({...computer, introduced: elt.target.value})}/>
                     <Input defaultValue={printDate({discontinued}.discontinued)}
-                           onChange={elt => setComputer({...computer, discontinued: dateToJSON(elt.target.value)})}/>
+                           onChange={elt => setComputer({...computer, discontinued: elt.target.value})}/>
 
                     <select onChange={elt => setComputer({...computer, company: companyToJSON(elt.target.value)})}>
 
