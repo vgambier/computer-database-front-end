5f49b750b29ea96fbf027c994a3c10bd3578b2fb
Victor Gambier <vgambier@excilys.com>
2020-08-12 14:15:33 +0200

    hotfix: fixed dropdown menu; removed duplicate button

--- src/Computer.js
+++ src/Computer.js
@@ -42,8 +42,6 @@ function Computer(props) {
             {!editMode ?
 
                 <>
-                    <Button onClick={() => setEditMode(!editMode)}>Edit</Button>
-
                     <td className="deleteMode">
                         <input type="checkbox" name="cb" className="cb" value={id}/>
                     </td>
